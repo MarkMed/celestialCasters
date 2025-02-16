813c914fbe8e16758186ae1624a25e2cc59af776
Marcos Á. Medina <marcmedi1@hotmail.com>
2025-02-16 01:59:58 -0300

    remove deprecated armorup effect and implement ccarmorup effects with updated configurations and animations

--- mods/CelestialCasters/stats/effects/ccarmorup/ccarmorup.lua
+++ mods/CelestialCasters/stats/effects/ccarmorup/ccarmorup.lua
@@ -1,30 +1,3 @@
--- function update(dt)
---     mcontroller.controlModifiers({
---         airJumpModifier = 0.9
---     })
---     self.tickTimer = self.tickTimer - dt
---     if self.tickTimer <= 0 then
---         self.tickTimer = self.tickTime
---         effect.addStatModifierGroup({{
---             stat = "jumpModifier",
---             amount = -0.1
---         }, {
---             stat = "protection",
---             amount = config.getParameter("extraArmorAmount", 10) -- 10 default extra armor?
---         } -- effect.addStatModifierGroup({{stat = "fireResistance", amount = 0.25}, {stat = "fireStatusImmunity", amount = 1}})
---         })
---     end
-    
--- end
-
--- function init()
---     animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
---     animator.setParticleEmitterActive("drips", true)
---     effect.setParentDirectives("fade=e0e0e0=0.3")
-
---     self.tickTime = 10.0
---     self.tickTimer = self.tickTime
--- end
 
 function update(dt)
     mcontroller.controlModifiers({
@@ -35,7 +8,7 @@ end
 function init()
     animator.setParticleEmitterOffsetRegion("drips", mcontroller.boundBox())
     animator.setParticleEmitterActive("drips", true)
-    effect.setParentDirectives("fade=e0e0e0=0.2")
+    effect.setParentDirectives(config.getParameter("colorWrap", "fade=e0e0e0=0.2"))
     
     -- status.setResource("energyRegenBlock", 1.0) -- prevents energy regen
 
