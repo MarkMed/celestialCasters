d351149991e74881731ad7eb28c0f0640c5ea1ff
Marcos Á. Medina <marcmedi1@hotmail.com>
2025-02-16 00:15:23 -0300

    add new ccfrost status effects: implement ccFrostAlly and ccFrostI with animations and dynamics values

---
+++ mods/CelestialCasters/stats/effects/ccfrost/ccfrost.lua
@@ -0,0 +1,59 @@
+function init()
+    animator.setParticleEmitterOffsetRegion("icetrail", mcontroller.boundBox())
+    animator.setParticleEmitterActive("icetrail", true)
+    effect.setParentDirectives(config.getParameter("colorWrap", "fade=00BBFF=0.15"))
+
+    script.setUpdateDelta(5)
+    -- Extraer valores desde el archivo JSON (o usar valores por defecto)
+    self.tickDamagePercentage = config.getParameter("tickDamagePercentage", 0.025) -- Daño por tick
+    self.tickTime = config.getParameter("tickTime", 1.0) -- Intervalo entre ticks
+    -- Inicializar temporizador de daño
+    self.tickTimer = self.tickTime
+
+    effect.addStatModifierGroup({
+      {
+        stat = "jumpModifier",
+        amount = config.getParameter("jumpModifier", -0.15)
+      },
+      {
+          stat = "protection",
+          amount = config.getParameter("extraArmorAmount", 0)
+      }
+    })
+end
+
+function update(dt)
+    -- Si el efecto de estado sigue activo y el jugador toca agua, se apaga
+    if effect.duration() and world.liquidAt({mcontroller.xPosition(), mcontroller.yPosition() - 1}) then
+        effect.expire()
+    end
+
+    -- Damage per tick in X seconds
+    self.tickTimer = self.tickTimer - dt
+    if self.tickTimer <= 0 then
+        self.tickTimer = self.tickTime
+        status.applySelfDamageRequest({
+            damageType = "IgnoresDef",
+            damage = math.floor(status.resourceMax("health") * self.tickDamagePercentage) + 1,
+            damageSourceKind = "ice",
+            sourceEntityId = entity.id()
+        })
+    end
+    -- AUMENTA CON EL TIEMPO!!!!!!!
+    -- effect.addStatModifierGroup({
+    --   {
+    --     stat = "protection",
+    --     amount = config.getParameter("extraArmorAmount", 0) -- 10 default extra armor?
+    --   } -- effect.addStatModifierGroup({{stat = "fireResistance", amount = 0.25}, {stat = "fireStatusImmunity", amount = 1}})
+    -- })
+
+    mcontroller.controlModifiers({
+        groundMovementModifier = config.getParameter("groundMovementModifier", 0.3),
+        speedModifier = config.getParameter("speedModifier", 0.75),
+        airJumpModifier = config.getParameter("airJumpModifier", 0.85)
+    })
+end
+
+function uninit()
+
+end
