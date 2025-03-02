19bbb8c783fe17ade9a71c5107f7e7f20e577f30
Marcos Á. Medina <marcmedi1@hotmail.com>
2025-03-02 04:54:29 -0300

    refactor Healing and ArmorModify effects for improved parameter handling

--- mods/CelestialCasters/stats/effects/basicStatusEffects.lua
+++ mods/CelestialCasters/stats/effects/basicStatusEffects.lua
@@ -6,32 +6,32 @@ BasicStatusEffects.Healing = {
     healAmount = 30,
     healingRate = 0
 }
-function BasicStatusEffects.Healing.init(config, currentStatus)
+function BasicStatusEffects.Healing.init(config)
     BasicStatusEffects.Healing.isInstant = config.getParameter("isInstant", false)
     BasicStatusEffects.Healing.healAmount = config.getParameter("healAmount", 30)
     if BasicStatusEffects.Healing.isInstant then
-        BasicStatusEffects.Healing.addHealth(BasicStatusEffects.Healing.healAmount, currentStatus)
+        BasicStatusEffects.Healing.addHealth(BasicStatusEffects.Healing.healAmount)
     else
-        local maxHealth = currentStatus.resourceMax("health")
-        local healthPercentage = maxHealth / 100
+        local maxHealth = status.resourceMax("health")
+        local healthPercentage = maxHealth / 100 --eg: 340 hp -> 1% = 3.4 -> healing 20% will heal 68 hp
         BasicStatusEffects.Healing.healingRate = (healthPercentage * BasicStatusEffects.Healing.healAmount)/(effect.duration() or 1)
     end
 end
-function BasicStatusEffects.Healing.update(dt, currentStatus)
+function BasicStatusEffects.Healing.update(dt)
     if not BasicStatusEffects.Healing.isInstant then
-        BasicStatusEffects.Healing.addHealth(BasicStatusEffects.Healing.healingRate * dt, currentStatus)
+        BasicStatusEffects.Healing.addHealth(BasicStatusEffects.Healing.healingRate * dt)
     end
 end
-function BasicStatusEffects.Healing.addHealth(healAmount, currentStatus)
-    currentStatus.modifyResource("health", healAmount)
+function BasicStatusEffects.Healing.addHealth(healAmount)
+    status.modifyResource("health", healAmount)
 end
 
 -- Armor Modify
 BasicStatusEffects.ArmorModify = {
-    armorAmount = 10
+    armorAmount = 0
 }
 function BasicStatusEffects.ArmorModify.init(config)
-    BasicStatusEffects.ArmorModify.armorAmount = config.getParameter("armorAmount", 10)
+    BasicStatusEffects.ArmorModify.armorAmount = config.getParameter("armorAmount", 0)
     BasicStatusEffects.ArmorModify.modifyArmor(BasicStatusEffects.ArmorModify.armorAmount)
 end
 function BasicStatusEffects.ArmorModify.modifyArmor(armorAmount)
