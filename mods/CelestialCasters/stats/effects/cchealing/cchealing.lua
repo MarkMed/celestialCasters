818f0b54944178b75aa14357cb69fd4af9e7328b
Marcos Á. Medina <marcmedi1@hotmail.com>
2025-03-02 04:54:58 -0300

    refactor cchealing statusEffect to utilize BasicStatusEffects for improved modularity

--- mods/CelestialCasters/stats/effects/cchealing/cchealing.lua
+++ mods/CelestialCasters/stats/effects/cchealing/cchealing.lua
@@ -1,3 +1,4 @@
+require "/stats/effects/basicStatusEffects.lua"
 function init()
 	self.isInstant = config.getParameter("isInstant", false)
 	animator.setParticleEmitterOffsetRegion("cchealing", mcontroller.boundBox())
@@ -6,28 +7,14 @@ function init()
     effect.setParentDirectives("fade=FFFFFF=0.1")
   
 	script.setUpdateDelta(5)
-  	self.currentHealth = status.resource("health")
-	self.maxHealth = status.resourceMax("health")
-	self.healthPercentage = self.maxHealth / 100 --eg: 340 hp -> 1% = 3.4 -> healing 20% will heal 68 hp
-	self.healAmount = config.getParameter("healAmount", 30)
-	if self.isInstant then
-		addHealt(self.healAmount)
-	else
-		self.healingRate = (self.healthPercentage * self.healAmount) / effect.duration()
-	end
+	
+	BasicStatusEffects.Healing.init(config, status)
   end
   
   function update(dt)
-	if not self.isInstant then
-		addHealt(self.healingRate * dt)
-	end
+	BasicStatusEffects.Healing.update(dt, status)
   end
   
   function uninit()
 	
   end
-
-  function addHealt(healAmount)
-	status.modifyResource("health", healAmount)
-  end
-  
